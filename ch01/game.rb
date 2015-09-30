eaebc3994d35a64eba149b21654b18603487476f
Douglas G. Allen <kb9agt@gmail.com>
2015-09-30 05:32:57 -0500

    Update game.rb

--- ch01/game.rb
+++ ch01/game.rb
@@ -20,6 +20,17 @@ get '/throw/:type' do
   if !@throws.include?(player_throw)
     halt 403, "You must throw one of the following: #{@throws}"
   end
+  
+  # now we can select a random throw for the computer
+  computer_throw = @throws.sample
+  # compare the player and computer throws to determine a winner
+  if player_throw == computer_throw
+    "You tied with the computer. Try again!"
+  elsif computer_throw == @defeat[player_throw]
+    "Nicely done; #{player_throw} beats #{computer_throw}!"
+  else
+    "Ouch; #{computer_throw} beats #{player_throw}. Better luck next time!"
+  end
 end
 
 
