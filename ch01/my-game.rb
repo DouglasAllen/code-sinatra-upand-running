af9206cec76a0f6944ec515e0d07ec41e7ef0d86
Douglas G. Allen <kb9agt@gmail.com>
2015-09-30 06:38:29 -0500

    Update my-game.rb

--- ch01/my-game.rb
+++ ch01/my-game.rb
@@ -1,9 +1,18 @@
 loop do
   @defeat = {rock: :scissors, paper: :rock, scissors: :paper}
   @throws = @defeat.keys
+  computer_throw = @throws.sample
   player_throw = gets.chomp.to_sym
   p player_throw
   if !@throws.include?(player_throw)
     p "wrong type"
+  end
+  
+  if player_throw == computer_throw
+    p "You tied with the computer. Try again!"
+  elsif computer_throw == @defeat[player_throw]
+    p "Nicely done; #{player_throw} beats #{computer_throw}!"
+  else
+    p "Ouch; #{computer_throw} beats #{player_throw}. Better luck next time!"
   end
 end
